01c4d3473b004858960fa92b74ae42b1c1f1f456
unknown <quentin.renaux@edu.univ-fcomte.fr>
2020-11-24 11:54:40 +0100

    service worker

--- sw.js
+++ sw.js
@@ -4,10 +4,9 @@ self.addEventListener('install', (e) => {
 
 var cacheName = 'PWA-v1';
 var appShellFiles = [
-  'PWA/',
-  'PWA/index.html',
-  'PWA/app.js',
-  'PWA/style.css',
+  'index.html',
+  'app.js',
+  'style.css',
   'incons/favicon.ico',
   'icons/icon-32.png',
   'icons/icon-64.png',
@@ -19,38 +18,3 @@ var appShellFiles = [
   'icons/icon-512.png',
   'icons/maskable_icon.png',
 ];
-
-var gamesImages = [];
-for(var i=0; i<games.length; i++) {
-  gamesImages.push('data/img/'+games[i].slug+'.jpg');
-}
-var contentToCache = appShellFiles.concat(gamesImages);
-
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
-    e.waitUntil(
-      caches.open(cacheName).then((cache) => {
-            console.log('[Service Worker] Caching all: app shell and content');
-        return cache.addAll(contentToCache);
-      })
-    );
-  });
-
-  self.addEventListener('fetch', (e) => {
-    console.log('[Service Worker] Fetched resource '+e.request.url);
-});
-
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-      caches.match(e.request).then((r) => {
-            console.log('[Service Worker] Fetching resource: '+e.request.url);
-        return r || fetch(e.request).then((response) => {
-                  return caches.open(cacheName).then((cache) => {
-            console.log('[Service Worker] Caching new resource: '+e.request.url);
-            cache.put(e.request, response.clone());
-            return response;
-          });
-        });
-      })
-    );
-  });
